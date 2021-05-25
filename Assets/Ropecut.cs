89f4015a9ecc11f389f38e207f7f1a10ca4b17c9
Sriharan <sriharaniam@gmail.com>
2021-05-25 20:12:55 +0530

    cut the rope

---
+++ Assets/Ropecut.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ropecut : MonoBehaviour
+{
+   
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if(hit.collider != null)
+            {
+                if(hit.collider.tag == "Link")
+                {
+                    Destroy(hit.collider.gameObject);
+                    //Destroy(hit.transform.parent.gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/script/Weight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weight : MonoBehaviour
+{
+    public float distancefromchain = 0.6f;
+    public void Connectrope(Rigidbody2D endrb)
+    {
+        HingeJoint2D joint =  gameObject.AddComponent<HingeJoint2D>();
+        joint.autoConfigureConnectedAnchor = false;
+        joint.connectedBody = endrb;
+        joint.anchor = Vector2.zero;
+        joint.connectedAnchor = new Vector2(0f, distancefromchain);
+    }
+}
